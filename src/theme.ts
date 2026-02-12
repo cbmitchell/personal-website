55dce4e278d4a72e6bcebea6524a785dcd5d5d4a
Chris Mitchell <bobtheawesomechicken@gmail.com>
2026-02-12 16:11:41 -0600

    Fill out site theme more to include all headers

--- src/theme.ts
+++ src/theme.ts
@@ -32,6 +32,26 @@ const theme = createTheme({
       fontSize: '2rem',
       fontWeight: 600,
     },
+    h2: {
+      fontSize: '1.75rem',
+      fontWeight: 600,
+    },
+    h3: {
+      fontSize: '1.5rem',
+      fontWeight: 600,
+    },
+    h4: {
+      fontSize: '1.25rem',
+      fontWeight: 600,
+    },
+    h5: {
+      fontSize: '1.1rem',
+      fontWeight: 600,
+    },
+    h6: {
+      fontSize: '1rem',
+      fontWeight: 600,
+    },
   },
   components: {
     MuiCssBaseline: {
